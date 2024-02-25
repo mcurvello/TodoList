e8ee4bb20e97f409f379901ad7120006967e21f8
Marcio Curvello <marciocurvello@gmail.com>
2024-02-25 10:58:47 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Marcio Curvello on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
